65c490728195d7be47316121ebd7c777b2152674
koppepan <koppepan0114@gmail.com>
2016-02-18 10:54:10 +0900

    MainSystemがnullの時は通信処理を行わない

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -6,32 +6,45 @@ public class GameManager : MonoBehaviour {
     public Player player;
     public Player enemy;
 
-	// Use this for initialization
-	void Start () {
+    MainSystem system;
 
-        MainSystem.Instance.Executor.OnReceivePlayerPosition += EnemyMove;
-	
+    void Start() {
+        system = MainSystem.Instance;
+        if (system == null)
+        {
+            return;
+        }
+        system.Executor.OnReceivePlayerPosition += EnemyMove;
 	}
 
     void OnDestroy()
     {
-        MainSystem.Instance.Executor.OnReceivePlayerPosition -= EnemyMove;
+        if (system == null)
+        {
+            return;
+        }
+        system.Executor.OnReceivePlayerPosition -= EnemyMove;
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        if(Input.GetKey(KeyCode.RightArrow))
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             player.Move(Player.Direction.Right);
-            MainSystem.Instance.Session.SendPlayerPosition(Player.Direction.Right);
+            if (system)
+            {
+                system.Session.SendPlayerPosition(Player.Direction.Right);
+            }
         }
-        else if(Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             player.Move(Player.Direction.Left);
-            MainSystem.Instance.Session.SendPlayerPosition(Player.Direction.Left);
+            if (system)
+            {
+                system.Session.SendPlayerPosition(Player.Direction.Left);
+            }
         }
-	
 	}
 
     void EnemyMove(Network.PlayerPosition msg)
